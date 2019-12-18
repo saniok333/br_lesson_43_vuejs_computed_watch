bc94539d8b1ff53d6dfb5960adf636d54e008749
saniok333 <alex.o.csc@gmail.com>
2019-12-18 13:19:45 +0200

    computed and watch

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,48 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ counter }}</h1>
+    <p>{{ getMessage() }} | {{ message }}</p>
+    <button @click="counter++">Increase</button> |
+    <button @click="counter--">Decrease</button>
+    <p>{{ counter2 }}</p>
+    <button @click="counter2++">Increase 2</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      counter: 0,
+      counter2: 0
+    };
+  },
+  methods: {
+    getMessage() {
+      console.log("Method called");
+      return this.counter < 5 ? "Меньше 5" : "Больше либо равно 5";
+    }
+  },
+  computed: {
+    message() {
+      console.log("Computed called");
+      return this.counter < 5 ? "Меньше 5" : "Больше либо равно 5";
+    }
+  },
+  watch: {
+    counter() {
+      let _this = this;
+      //console.log("Counter:", value);
+      //console.log("Counter:", this.counter);
+      if (this.counter <= -10 || this.counter >= 10) {
+        setTimeout(() => {
+          console.log(this);
+          console.log("Counter:", this.counter);
+          this.counter = 0;
+        }, 1000);
+      }
+    }
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
